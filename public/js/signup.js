3313e8743f75a319df9db2af7967d88ac8e84a17
kivi3004 <kirtyvijay999@gmail.com>
2021-04-14 23:54:19 +0530

    Searching using like keyword

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,83 +1,83 @@
-function isValid(str){
-    var regex = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g;
-      return regex.test(str);
+function isValid(str) {
+  var regex = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g;
+  return regex.test(str);
+}
+function checkPassword(e) {
+  var keynum;
+  if (window.event) {
+    keynum = e.keyCode;
+  } else if (e.which) {
+    keynum = e.which;
   }
-  function checkPassword(e) {
-    var keynum;
-    if(window.event) {                
-      keynum = e.keyCode;
-    } else if(e.which){                
-      keynum = e.which;
-    }
-    let ch = String.fromCharCode(keynum);
-    const val = document.getElementById('password').value;
-    if (val.length >= 8 && val.length <= 16) {
-      document.getElementById('charactersLong').style.color = 'green';
-    }
-    else {
-      document.getElementById('charactersLong').style.color = 'red';
-    }
-    if (isValid(ch)) {
-      document.getElementById('special').style.color = 'green';
-    }
-    if (!(ch >= '0' && ch <= '9') && ch == ch.toUpperCase()) {
-      document.getElementById('uppercase').style.color = 'green';
-    }
-    if (!(ch >= '0' && ch <= '9') && ch == ch.toLowerCase()) {
-      document.getElementById('lowercase').style.color = 'green';
-    }
+  let ch = String.fromCharCode(keynum);
+  const val = document.getElementById('password').value;
+  if (val.length >= 8 && val.length <= 16) {
+    document.getElementById('charactersLong').style.color = 'green';
   }
-  const keyDown = document.getElementById('password');
-    keyDown.addEventListener('keydown', (event) => {
-      const key = event.key;
-      if (key === 'Backspace' || key === 'Delete') {
-        let str = document.getElementById('password').value;
-        str = str.substring(0, str.length - 2);
-        let uppercase = 0;
-        let lowercase = 0;
-        let special = 0;
+  else {
+    document.getElementById('charactersLong').style.color = 'red';
+  }
+  if (isValid(ch)) {
+    document.getElementById('special').style.color = 'green';
+  }
+  if (!(ch >= '0' && ch <= '9') && ch == ch.toUpperCase()) {
+    document.getElementById('uppercase').style.color = 'green';
+  }
+  if (!(ch >= '0' && ch <= '9') && ch == ch.toLowerCase()) {
+    document.getElementById('lowercase').style.color = 'green';
+  }
+}
+const keyDown = document.getElementById('password');
+keyDown.addEventListener('keydown', (event) => {
+  const key = event.key;
+  if (key === 'Backspace' || key === 'Delete') {
+    let str = document.getElementById('password').value;
+    str = str.substring(0, str.length - 2);
+    let uppercase = 0;
+    let lowercase = 0;
+    let special = 0;
 
-        if (str.length >= 8 && str.length <= 16) document.getElementById('charactersLong').style.color = 'green';
-        else document.getElementById('charactersLong').style.color = 'red';
+    if (str.length >= 8 && str.length <= 16) document.getElementById('charactersLong').style.color = 'green';
+    else document.getElementById('charactersLong').style.color = 'red';
 
-        for (let i = 0; i < str.length; i++) {
-          if (!(str[i] >= '0' && str[i] <= '9') && str[i] == str[i].toUpperCase()) uppercase = 1;
-          if (!(str[i] >= '0' && str[i] <= '9') && str[i] == str[i].toLowerCase()) lowercase = 1;
-          if (isValid(str[i])) special = 1;
-        }
-
-        if (uppercase === 1) document.getElementById('uppercase').style.color = 'green';
-        else document.getElementById('uppercase').style.color = 'red';
-
-        if (lowercase === 1) document.getElementById('lowercase').style.color = 'green';
-        else document.getElementById('lowercase').style.color = 'red';
-
-        if (special === 1) document.getElementById('special').style.color = 'green';
-        else document.getElementById('special').style.color = 'red';
-      }
-    })
-
-    function setColor() {
-        document.getElementById('charactersLong').style.color = 'red';
-        document.getElementById('uppercase').style.color = 'red';
-        document.getElementById('lowercase').style.color = 'red';
-        document.getElementById('special').style.color = 'red';
+    for (let i = 0; i < str.length; i++) {
+      if (!(str[i] >= '0' && str[i] <= '9') && str[i] == str[i].toUpperCase()) uppercase = 1;
+      if (!(str[i] >= '0' && str[i] <= '9') && str[i] == str[i].toLowerCase()) lowercase = 1;
+      if (isValid(str[i])) special = 1;
     }
 
-    function checkValidation() {
-        const characters = document.getElementById('charactersLong').style.getPropertyValue("color");
-        const uppercase  = document.getElementById('uppercase').style.color;
-        const lowercase  = document.getElementById('lowercase').style.color;
-        const special    = document.getElementById('special').style.color;
-        const password    = document.getElementById('password').value;
-        const confirmPassword    = document.getElementById('passwordConfirm').value;
-        if (uppercase === 'green' && characters === 'green' && lowercase === 'green' && special === 'green' && password === confirmPassword) {
-            return true;
-        }
-        if (uppercase === 'red') alert('Password must contain atleast one Upper case Character.');
-        else if (lowercase === 'red') alert('Password must contain atleast one Lower case Character.');
-        else if (special === 'red') alert('Password must contain atleast one Special Character.');
-        else if(characters === 'red') alert('Password must be atleast 8-16 characters long.');
-        else alert("Password doesn't matched");
-        return false;
-    }
+    if (uppercase === 1) document.getElementById('uppercase').style.color = 'green';
+    else document.getElementById('uppercase').style.color = 'red';
+
+    if (lowercase === 1) document.getElementById('lowercase').style.color = 'green';
+    else document.getElementById('lowercase').style.color = 'red';
+
+    if (special === 1) document.getElementById('special').style.color = 'green';
+    else document.getElementById('special').style.color = 'red';
+  }
+})
+
+function setColor() {
+  document.getElementById('charactersLong').style.color = 'red';
+  document.getElementById('uppercase').style.color = 'red';
+  document.getElementById('lowercase').style.color = 'red';
+  document.getElementById('special').style.color = 'red';
+}
+
+function checkValidation() {
+  const characters = document.getElementById('charactersLong').style.getPropertyValue("color");
+  const uppercase = document.getElementById('uppercase').style.color;
+  const lowercase = document.getElementById('lowercase').style.color;
+  const special = document.getElementById('special').style.color;
+  const password = document.getElementById('password').value;
+  const confirmPassword = document.getElementById('passwordConfirm').value;
+  if (uppercase === 'green' && characters === 'green' && lowercase === 'green' && special === 'green' && password === confirmPassword) {
+    return true;
+  }
+  if (uppercase === 'red') alert('Password must contain atleast one Upper case Character.');
+  else if (lowercase === 'red') alert('Password must contain atleast one Lower case Character.');
+  else if (special === 'red') alert('Password must contain atleast one Special Character.');
+  else if (characters === 'red') alert('Password must be atleast 8-16 characters long.');
+  else alert("Password doesn't matched");
+  return false;
+}
--- routes/userview.js
+++ routes/userview.js
@@ -11,15 +11,15 @@ dotenv.config();
 
 //add some urls
 
-router.post('/osr/fetchdetails', authenticateToken ,(req, res) => {
+router.post('/osr/fetchdetails', authenticateToken, (req, res) => {
     const res_id = req.body.res_id;
     db.execute("SELECT * FROM RESOURCES WHERE res_id = ?", [res_id])
-    .then(rs => {
-        var result = rs[0];
-        console.log(result);
-        res.send( {data: result} );
-    })
-    .catch(err => console.log(err))
+        .then(rs => {
+            var result = rs[0];
+            console.log(result);
+            res.send({ data: result });
+        })
+        .catch(err => console.log(err))
 });
 
 router.post('/osr/update', authenticateToken, (req, res) => {
@@ -33,109 +33,130 @@ router.post('/osr/update', authenticateToken, (req, res) => {
         + currentdate.getMinutes() + ":"
         + currentdate.getSeconds();
     db.execute("UPDATE resources SET description = ?, date = ?  WHERE res_id = ?", [description, datetime, res_id])
-    .then(rs => {
-        console.log(rs);
-        res.redirect('/osr/activities');
-    }) 
-    .catch(err => console.log(err))
+        .then(rs => {
+            console.log(rs);
+            res.redirect('/osr/activities');
+        })
+        .catch(err => console.log(err))
 });
 
 router.post('/osr/delete', authenticateToken, (req, res) => {
     const res_id = req.body.res_id;
     db.execute("DELETE FROM ratings WHERE res_id = ?", [res_id])
-    .then(rs => {
-        db.execute("DELETE FROM resources WHERE res_id = ?", [res_id])
-            .then(res1 => {
-                res.redirect('/osr/activities');
-            })
-    })
-    .catch(err => console.log(err))
+        .then(rs => {
+            db.execute("DELETE FROM resources WHERE res_id = ?", [res_id])
+                .then(res1 => {
+                    res.redirect('/osr/activities');
+                })
+        })
+        .catch(err => console.log(err))
 });
 
 router.get('/osr/userview', authenticateToken, (req, res) => {
-    res.render("index", { message: "Home Page", flag : true })
+    res.render("index", { message: "Home Page", flag: true })
 })
 router.get('/osr/addResources', authenticateToken, (req, res) => {
-    res.render("addResources", { message: "Add Resources", flag : true });
+    res.render("addResources", { message: "Add Resources", flag: true });
 })
 router.post('/osr/dashboard', authenticateToken, (req, res) => {
     const id = req.user.id;
     db.execute("SELECT r.res_id, r.user_id, r.title, r.link, r.content, r.date, r.description, r.likes, r.unlikes, IFNULL(rating.rate, -2) as rate, IFNULL(rating.rating_id, 0) as rating_id  from resources as r LEFT JOIN ratings as rating on r.res_id=rating.res_id where approval=?", ['approve'])
         .then((rs) => {
             let result = rs[0];
-            res.render("home", {data : result, id,  message: "User Account", flag : true, filter : "Recently Added" })
+            res.render("home", { data: result, id, message: "User Account", flag: true, filter: "Recently Added" })
         })
-        
+
         .catch(err => console.log(err))
 })
 router.post('/osr/search', authenticateToken, (req, res) => {
     const id = req.user.id;
-    const action = req.body.action;
+    var action = req.body.action;
     console.log(req.body.action)
-    if(action === "Recently Added"){
-        db.execute("SELECT r.res_id, r.user_id, r.title, r.link, r.content, r.date, r.description, r.likes, r.unlikes, r.Comments, IFNULL(rating.rate, -2) as rate, IFNULL(rating.rating_id, 0) as rating_id  from resources as r LEFT JOIN ratings as rating on r.res_id=rating.res_id where approval=? ORDER BY date DESC", ['approve'])
+    if (req.body.Search) {
+
+        const search = req.body.Search;
+        //console.log(search)
+        db.execute("SELECT r.res_id, r.user_id, r.title, r.link, r.content, r.date, r.description, r.likes, r.unlikes, r.Comments, IFNULL(rating.rate, -2) as rate, IFNULL(rating.rating_id, 0) as rating_id  from resources as r LEFT JOIN ratings as rating on r.res_id=rating.res_id where approval=? and (description LIKE ? or title LIKE ?) ORDER BY date DESC", ['approve', '%' + search + '%', '%' + search + '%'])
             .then((rs) => {
                 let result = rs[0];
-                res.render("home", {data : result, id,  message: "User Account", flag : true, filter : req.body.action})
+                // console.log(result)
+                res.render("home", { data: result, id, message: "User Account", flag: true, filter: action, search })
             })
-            
+
             .catch(err => console.log(err))
     }
-    else if(action === "Most Voted"){
-        db.execute("SELECT r.res_id, r.user_id, r.title, r.link, r.content, r.date, r.description, r.likes, r.unlikes, r.Comments, IFNULL(rating.rate, -2) as rate, IFNULL(rating.rating_id, 0) as rating_id  from resources as r LEFT JOIN ratings as rating on r.res_id=rating.res_id where approval=? ORDER BY likes DESC", ['approve'])
-            .then((rs) => {
-                let result = rs[0];
-                res.render("home", {data : result, id,  message: "User Account", flag : true, filter : req.body.action})
-            })
-            
-            .catch(err => console.log(err))
-    }
-    
     else {
-        db.execute("SELECT r.res_id, r.user_id, r.title, r.link, r.content, r.date, r.description, r.likes, r.unlikes, r.Comments, IFNULL(rating.rate, -2) as rate, IFNULL(rating.rating_id, 0) as rating_id  from resources as r LEFT JOIN ratings as rating on r.res_id=rating.res_id where approval=? ORDER BY comments  DESC", ['approve'])
-            .then((rs) => {
-                let result = rs[0];
-                res.render("home", {data : result, id,  message: "User Account", flag : true, filter : req.body.action})
-            })
-            
-            .catch(err => console.log(err))
+        const search = action.substr(action.lastIndexOf('_') + 1, action.length)
+        action = action.substr(0, action.lastIndexOf('_'))
+        if (action === "Recently Added") {
+            //console.log(search)
+            db.execute("SELECT r.res_id, r.user_id, r.title, r.link, r.content, r.date, r.description, r.likes, r.unlikes, r.Comments, IFNULL(rating.rate, -2) as rate, IFNULL(rating.rating_id, 0) as rating_id  from resources as r LEFT JOIN ratings as rating on r.res_id=rating.res_id where approval=? and (description LIKE ? or title LIKE ?) ORDER BY date DESC", ['approve', '%' + search + '%', '%' + search + '%'])
+                .then((rs) => {
+                    let result = rs[0];
+                    console.log(result)
+                    res.render("home", { data: result, id, message: "User Account", flag: true, filter: action, search })
+                })
+
+                .catch(err => console.log(err))
+        }
+        else if (action == "Most Voted") {
+            db.execute("SELECT r.res_id, r.user_id, r.title, r.link, r.content, r.date, r.description, r.likes, r.unlikes, r.Comments, IFNULL(rating.rate, -2) as rate, IFNULL(rating.rating_id, 0) as rating_id  from resources as r LEFT JOIN ratings as rating on r.res_id=rating.res_id where approval=? and (description LIKE ? or title LIKE ?) ORDER BY likes DESC", ['approve', '%' + search + '%', '%' + search + '%'])
+                .then((rs) => {
+                    let result = rs[0];
+                    // console.log(result)
+                    res.render("home", { data: result, id, message: "User Account", flag: true, filter: action, search })
+                })
+
+                .catch(err => console.log(err))
+        }
+        else{
+            db.execute("SELECT r.res_id, r.user_id, r.title, r.link, r.content, r.date, r.description, r.likes, r.unlikes, r.Comments, IFNULL(rating.rate, -2) as rate, IFNULL(rating.rating_id, 0) as rating_id  from resources as r LEFT JOIN ratings as rating on r.res_id=rating.res_id where approval=? and (description LIKE ? or title LIKE ?) ORDER BY comments DESC", ['approve', '%' + search + '%', '%' + search + '%'])
+                .then((rs) => {
+                    let result = rs[0];
+                    // console.log(action)
+                    res.render("home", { data: result, id, message: "User Account", flag: true, filter: action, search })
+                })
+
+                .catch(err => console.log(err))
+
+        }
     }
 })
 
 
 router.get('/osr/activities', authenticateToken, (req, res) => {
     const id = req.user.id;
-    db.execute("SELECT r.res_id, r.user_id, r.title, r.link, r.content, r.date, r.description, r.likes, r.unlikes, r.Comments , IFNULL(rating.rate, -2) as rate, IFNULL(rating.rating_id, 0) as rating_id  from resources as r LEFT JOIN ratings as rating on r.res_id=rating.res_id where r.user_id=? and approval=?",[id, 'approve'])
+    db.execute("SELECT r.res_id, r.user_id, r.title, r.link, r.content, r.date, r.description, r.likes, r.unlikes, r.Comments , IFNULL(rating.rate, -2) as rate, IFNULL(rating.rating_id, 0) as rating_id  from resources as r LEFT JOIN ratings as rating on r.res_id=rating.res_id where r.user_id=? and approval=?", [id, 'approve'])
         .then((rs) => {
             let result = rs[0];
-            res.render("dashboard", {data : result, id, message: "User Account", flag : true })
+            res.render("dashboard", { data: result, id, message: "User Account", flag: true })
         })
-        
+
         .catch(err => console.log(err))
 
 })
-router.get('/osr/request', authenticateToken, (req,res) =>{
+router.get('/osr/request', authenticateToken, (req, res) => {
     var id = req.user.id
     db.execute('select * from resources where approval = ? and user_id = ?', ['pending', id])
-    .then(res1 => {
-        console.log(res1[0])
-        res.render("pendingRequest", {flag : true, data: res1[0], filter:"pending"})
-    })
-    .catch(err =>{
-        console.log("err");
-    })
+        .then(res1 => {
+            console.log(res1[0])
+            res.render("pendingRequest", { flag: true, data: res1[0], filter: "pending" })
+        })
+        .catch(err => {
+            console.log("err");
+        })
 })
 
-router.post('/osr/request', authenticateToken, (req,res) =>{
+router.post('/osr/request', authenticateToken, (req, res) => {
     var id = req.user.id
     db.execute('select * from resources where approval = ? and user_id = ?', [req.body.action, id])
-    .then(res1 => {
-        console.log(res1[0])
-        res.render("pendingRequest", {flag : true, data: res1[0], filter:req.body.action})
-    })
-    .catch(err =>{
-        console.log("err");
-    })
+        .then(res1 => {
+            console.log(res1[0])
+            res.render("pendingRequest", { flag: true, data: res1[0], filter: req.body.action })
+        })
+        .catch(err => {
+            console.log("err");
+        })
 })
 
 
@@ -184,21 +205,21 @@ router.post('/osr/resources', authenticateToken, (req, res) => {
         }
         else if (!req.file) {
             db.execute('INSERT INTO resources(user_id, title, link, content, date, description) values(?, ?, ?, ?, ?, ?)',
-            [req.user.id, req.body.title, req.body.url, "NO FILE CHOOSEN", datetime, req.body.description])
-            .then((resources) => {
-                if (resources) {
-                    const result = resources[0];
-                    console.log(result.insertId);
-                    res.redirect("/osr/activities");
-                    const res_id = result.insertId;
-                    // db.execute('INSERT INTO ratings(res_id, user_id, rate) values(?, ?, ?)', [res_id, req.user.id, 0])
-                    //     .then(res1 => {
-                    //         res.redirect("/osr/activities");
-                    //     })
-                    //     .catch(err => console.log(err));
-                }
-            })
-            .catch(err => console.log(err));
+                [req.user.id, req.body.title, req.body.url, "NO FILE CHOOSEN", datetime, req.body.description])
+                .then((resources) => {
+                    if (resources) {
+                        const result = resources[0];
+                        console.log(result.insertId);
+                        res.redirect("/osr/activities");
+                        const res_id = result.insertId;
+                        // db.execute('INSERT INTO ratings(res_id, user_id, rate) values(?, ?, ?)', [res_id, req.user.id, 0])
+                        //     .then(res1 => {
+                        //         res.redirect("/osr/activities");
+                        //     })
+                        //     .catch(err => console.log(err));
+                    }
+                })
+                .catch(err => console.log(err));
             console.log("NO file")
             // res.send('Please select an file to upload');
         }
@@ -208,25 +229,25 @@ router.post('/osr/resources', authenticateToken, (req, res) => {
         else if (err) {
             return res.send(err);
         }
-        else{
+        else {
             db.execute('INSERT INTO resources(user_id, title, link, content, date, description) values(?, ?, ?, ?, ?, ?)',
-            [req.user.id, req.body.title, req.body.url, req.file.originalname, datetime, req.body.description])
-            .then((resources) => {
-                if (resources) {
-                    const result = resources[0];
-                    console.log(result.insertId);
-                    res.redirect("/osr/activities");
-                    const res_id = result.insertId;
-                    // db.execute('INSERT INTO ratings(res_id, user_id, rate) values(?, ?, ?)', [res_id, req.user.id, 0])
-                    //     .then(res1 => {
-                    //         res.redirect("/osr/activities");
-                    //     })
-                    //     .catch(err => console.log(err));
-                }
-            })
-            .catch(err => console.log(err));
+                [req.user.id, req.body.title, req.body.url, req.file.originalname, datetime, req.body.description])
+                .then((resources) => {
+                    if (resources) {
+                        const result = resources[0];
+                        console.log(result.insertId);
+                        res.redirect("/osr/activities");
+                        const res_id = result.insertId;
+                        // db.execute('INSERT INTO ratings(res_id, user_id, rate) values(?, ?, ?)', [res_id, req.user.id, 0])
+                        //     .then(res1 => {
+                        //         res.redirect("/osr/activities");
+                        //     })
+                        //     .catch(err => console.log(err));
+                    }
+                })
+                .catch(err => console.log(err));
         }
-        
+
     });
 });
 
